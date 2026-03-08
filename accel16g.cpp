1e6321a3d02459543775bd7af48fa1216d0ccbcd
kelakuta <34112098+kelakuta@users.noreply.github.com>
2026-03-08 18:52:17 +0000

    Update accel16g.cpp

--- accel16g.cpp
+++ accel16g.cpp
@@ -7,10 +7,8 @@ namespace accel16g {
     void enable16g() {
         const uint8_t CTRL_REG4_A = 0x23;
         uint8_t data[2];
-
         data[0] = CTRL_REG4_A;
-        data[1] = 0x30; // ±16g
-
+        data[1] = 0x30;
         uBit.i2c.write(0x32, (char*)data, 2);
     }
 }
